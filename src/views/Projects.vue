f19f7f51d2a31be6aa80ea5bb5d14b1f93be811b
Vanilna <lesyalp@gmail.com>
2020-03-03 10:51:14 +0100

    style project component for desktop

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -5,12 +5,43 @@
     <Project
       v-for="(project, index) in projects"
       :isActive="index === activeId"
+      class="poject"
       :project="project"
       :key="project.name"
     />
   </main>
 </template>
-<style scoped></style>
+<style scoped>
+.content {
+  max-width: 1200px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 15% repeat(5, 1fr);
+  grid-template-rows: auto;
+}
+.decorText {
+  grid-column: 1 / 2;
+  grid-row: 1 / 4;
+  text-transform: uppercase;
+  font-size: 3rem;
+  letter-spacing: 0.5rem;
+  writing-mode: vertical-rl;
+  text-orientation: mixed;
+  padding: 0;
+  margin: 0;
+  justify-self: center;
+}
+.iframe {
+  grid-column: 2 / 5;
+  grid-row: 1 / 4;
+  border: 1px solid #ffffff;
+  max-height: 65vh;
+}
+.project {
+  grid-column: 5 / 7;
+  grid-row: span 1;
+}
+</style>
 <script>
 import Project from "@/components/Project.vue";
 
