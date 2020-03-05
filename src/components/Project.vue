36e9155e24c07e912f00f62fa7b16fc4f58c7847
Vanilna <lesyalp@gmail.com>
2020-03-05 16:29:31 +0100

    style active project

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,26 +1,62 @@
 <template>
   <div class="project" :class="{ active: isActive }">
-    <div class="heading">
-      <h2 class="name">{{ project.name }}</h2>
-      <img
-        v-for="techSrc in techSrces"
-        :src="techSrc"
-        :key="techSrc"
-        class="technologie"
-      />
-    </div>
-    <p class="description">{{ project.description }}</p>
-    <div class="links">
-      <a class="link" :href="project.web" target="_blank">web</a>
-      <a class="link" :href="project.code" target="_blank">code</a>
+    <arrow v-if="isActive" class="arrow" />
+    <div class="content" :class="{ active: isActive }">
+      <div class="heading">
+        <h2 class="name">{{ project.name }}</h2>
+        <img
+          v-for="techSrc in techSrces"
+          :src="techSrc"
+          :key="techSrc"
+          class="technologie"
+        />
+      </div>
+      <p class="description">{{ project.description }}</p>
+      <div class="links">
+        <a class="link" :href="project.web" target="_blank">web</a>
+        <a class="link" :href="project.code" target="_blank">code</a>
+      </div>
     </div>
     <hr class="line" />
   </div>
 </template>
+<script>
+import Arrow from "../components/UI/Arrow.vue";
+
+export default {
+  name: "Project",
+  props: {
+    project: Object,
+    isActive: Boolean
+  },
+  components: { Arrow },
+  computed: {
+    techSrces: function() {
+      const srces = this.project.technologies.map(tech =>
+        require(`../assets/technologies/${tech}.svg`)
+      );
+      return srces;
+    }
+  }
+};
+</script>
 <style scoped>
 .project {
   margin-left: 2rem;
   margin-right: 1rem;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.arrow {
+  height: 10rem;
+  width: 2rem;
+  stroke-width: 5rem;
+  stroke: #ffd500;
+  flex-basis: 7%;
+}
+.content.active {
+  flex-basis: 90%;
 }
 .heading {
   display: flex;
@@ -58,6 +94,7 @@
 .line {
   border: 0;
   height: 1px;
+  flex-basis: 100%;
   background-image: linear-gradient(
     to right,
     rgba(255, 213, 0, 0),
@@ -66,20 +103,3 @@
   );
 }
 </style>
-<script>
-export default {
-  name: "Project",
-  props: {
-    project: Object,
-    isActive: Boolean
-  },
-  computed: {
-    techSrces: function() {
-      const srces = this.project.technologies.map(tech =>
-        require(`../assets/technologies/${tech}.svg`)
-      );
-      return srces;
-    }
-  }
-};
-</script>
