6858e932fe3582ce85ce6db5972befff6219b78e
Vanilna <lesyalp@gmail.com>
2020-05-26 15:08:04 +0200

    took projects info to it's own file

---
+++ src/data/projects.js
@@ -0,0 +1,97 @@
+export const projects = [
+  {
+    name: "Mars Rovers",
+    technologies: [
+      "React",
+      "Styled Components",
+      "Storybook",
+      "Jest",
+      "React Testing Library",
+      "GSAP",
+    ],
+    description: `<p>The project was build to practice some new technologies in my stack and also to <span class="emfasized">learn to test</span>.</p>
+      <p>Site fetches info about Mars rovers and photos made by them from NASA API and displays them.</p>
+      <p>Some details:</p>
+      <ul>
+      <li>using <span class="emfasized">atomic design</span> patter and <span class="emfasized">Storybook</span>;</li>
+      <li>styling components with <span class="emfasized">Styled Components</span>;</li>
+      <li>working with <span class="emfasized">REST API</span>;</li>
+      <li>unit and integration tests with <span class="emfasized">Jest</span> and <span class="emfasized">React testing library</span>;</li>
+      <li>animating with <span class="emfasized">GSAP</span>;</li>
+      <li>handle image <span class="emfasized">lazy loading</span> and adding content on scroll.</li>
+      </ul>`,
+    web: "https://rovers.netlify.com",
+    code: "https://github.com/Vanilna/mars",
+  },
+  {
+    name: "Art gallery",
+    technologies: ["HTML5", "CSS3", "JavaScript", "Webpack"],
+    description: `<p>An art gallery for a family of famous Ukrainian artists.</p>
+      <p>The site was written using almost only <span class="emfasized">HTML, CSS, and JavaScript</span>.</p>
+      <p>Some details:</p>
+      <ul>
+      <li>I've <span class="emfasized">deepened my knowledge about fundamental technologies</span>;</li>
+      <li>I've practiced <span class="emfasized">DOM manipulations</span>;</li>
+      <li>the site was split into modules and <span class="emfasized">build with Webpack</span>;</li>
+      <li>I've learned <span class="emfasized">code splitting</span> and <span class="emfasized">lazy loading</span> techniques;</li>
+      <li>the biggest challenge was <span class="emfasized">image optimization</span>, which I've handled using Cloudinary.</li>
+      </ul>`,
+    web: "https://vytiahlovski.netlify.com/",
+    code: "https://github.com/Vanilna/Vytiahlovski",
+  },
+  {
+    name: "Quiz",
+    technologies: ["React", "Redux", "CSS3"],
+    description:
+      "<p>Little quiz game to practice React, Redux and working with REST API.</p>",
+    web: "https://quiz-vanilna.netlify.com/",
+    code: "https://github.com/Vanilna/quiz",
+  },
+  {
+    name: "Check-in form",
+    technologies: ["HTML5", "Bootstrap", "JavaScript"],
+    description:
+      "<p>A check-in form for hotel guest.</p><p>Made for practising Semantik HTML and Bootstrap.</p>",
+    web: "https://hotel-check-in.netlify.com/",
+    code: "https://github.com/Vanilna/online-check-in-form",
+  },
+  {
+    name: "Advanced React form",
+    technologies: ["React", "CSS3"],
+    description:
+      "<p>An advanced form made for practising form validation in React.</p>",
+    web: "https://vanilna-react-form.netlify.com",
+    code: "https://github.com/Vanilna/react-form",
+  },
+  {
+    name: "Mobile navigation",
+    technologies: ["SASS", "JavaScript", "HTML5"],
+    description:
+      "<p>Project for practising SASS. Design created by the project of Aurélien Salomon for Orizon.</p>",
+    web: "https://vanilna.github.io/mobileNav/app/index.html",
+    code: "https://github.com/Vanilna/mobileNav",
+  },
+  {
+    name: "Template from Dribble",
+    technologies: ["Bootstrap", "HTML5"],
+    description:
+      "<p>Copied design from dribble template to practise Bootstrap.</p>",
+    web: "https://vanilna.github.io/template-bootstrap/build/index.html",
+    code: "https://github.com/Vanilna/template-bootstrap",
+  },
+  {
+    name: "Memory Game",
+    technologies: ["React", "CSS3"],
+    description: "<p>Simple game to practice React and CSS animations.</p>",
+    web: "https://memory-game-vanilna.netlify.com/",
+    code: "https://github.com/Vanilna/memory-game",
+  },
+  {
+    name: "Kate Blog",
+    technologies: ["Bootstrap", "HTML5"],
+    description:
+      "<p>Copied blog from the original for learning reasons (learning Bootstrap).</p>",
+    web: "https://vanilna.github.io/Kate-blog/",
+    code: "https://github.com/Vanilna/Kate-blog",
+  },
+];
